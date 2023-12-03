8e678f5d4b90c541fa69e6d0b747f13379d33ddc
sora-szk <sora0.kizmi@gmail.com>
2023-12-03 20:26:21 +0900

    fix: update connection settings

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,11 @@ import { createApp } from './app'
 const startServer = async () => {
     const port = process.env.API_PORT
     const app = await createApp()
-    app.listen(port)
+    const server = app.listen(port)
+    server.keepAliveTimeout = 800 * 1000
+    server.headersTimeout = 900 * 1000
+    console.log(`keepAliveTimeout: ${server.keepAliveTimeout}`)
+    console.log(`headersTimeout: ${server.headersTimeout}`)
     console.log(`Server listening on port ${port}`)
 }
 
